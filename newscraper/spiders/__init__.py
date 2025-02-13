408f12986ee603e7c470a8a4123dee364b65b1a2
Pradyumn Prakash Srivastava <pradyumn.srivast-int@tmrw.in>
2025-02-13 16:06:25 +0530

    add base_scraper and operations

--- newscraper/spiders/__init__.py
+++ newscraper/spiders/__init__.py
@@ -1,4 +1,18 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+import scrapy
+import os
+import json
+
+class BaseSpider(scrapy.Spider):  # Make sure the class name is capitalized
+    def __init__(self, *args, **kwargs):
+        super(BaseSpider, self).__init__(*args, **kwargs)  # Fixed reference to the class
+
+        # Get the absolute path to the current file's directory
+        home = os.path.dirname(os.path.abspath(__file__))        
+        selectors_path = os.path.join(home, 'selectors.json')  # Use os.path.join for cross-platform compatibility
+        
+        # Load the selectors.json configuration
+        with open(selectors_path, 'r') as f:
+            self.config = json.load(f)
+
+        self.start_urls = self.config.get('start_urls', [])
+        self.selectors = self.config.get('selectors', {})
